773ceae2113bffb10ba7262d05d550f5be7a7cb8
talktodari45-sketch <talktodari45@gmail.com>
2025-09-13 20:49:59 +0530

    first chat without memory

--- Longchain1.py
+++ Longchain1.py
@@ -1,33 +1,21 @@
-from langchain_groq import ChatGroq
-import getpass
-import os
-
-import os
 from dotenv import load_dotenv
 import os
 
+
+
+
+from langchain_groq import ChatGroq
 load_dotenv()
-API_KEY = os.environ.get("GROQ_API_KEY")
-if not API_KEY:
-    raise RuntimeError("GROQ_API_KEY not found in environment")
+api_key = os.getenv("GROQ_API_KEY")
 
-print(33)
-llm = ChatGroq(
-    model="deepseek-r1-distill-llama-70b",
-    temperature=0,
-    max_tokens=None,
-    reasoning_format="parsed",
-    timeout=None,
-    max_retries=2,
-    # other params...
-)
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence.",
-    ),
-    ("human", "I love programming."),
-]
-ai_msg = llm.invoke(messages)
-print(ai_msg)
+llm = ChatGroq(api_key=api_key, model="llama-3.3-70b-versatile")
+
+
+def simple_chat_without_memory(user_query):
+    response = llm.invoke(user_query)
+    return response.content
+
+
+response1 = simple_chat_without_memory("Hi")
+print(response1)
